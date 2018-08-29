f9f3a1f5856b48a257927df1cca0bb7bba0cb865
Yancey <developer@yanceyleo.com>
2018-08-30 01:32:19 +0800

    finish 70% of Archive page

--- src/containers/Archive/Archive.js
+++ src/containers/Archive/Archive.js
@@ -1,6 +1,8 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import styles from './archive.module.css';
 import { GET } from '../../https/axios';
+
+const Mock = require('mockjs');
 
 class Archive extends Component {
   constructor(props) {
@@ -14,62 +16,102 @@ class Archive extends Component {
   }
 
   componentDidMount() {
-    this.getData();
+    this.getFakeData();
   }
 
   componentWillUnmount() {
   }
 
-  getData() {
-    GET('/articles', {})
-      .then((res) => {
-        this.setState({
-          data: res.data,
-        });
-      })
-      .catch((error) => {
-        console.log(error.message);
-      });
+  getFakeData() {
+    const data = Mock.mock({
+      'data|2-3': [
+        {
+          year: '20@integer(10, 18)',
+          'data|1-12': [
+            {
+              month: '@integer(1, 12)',
+              'days|0-6': [
+                {
+                  day: '@integer(1, 28)',
+                  title: '@ctitle(10, 30)',
+                  like_count: '@integer(0, 500)',
+                  comment_count: '@integer(0, 500)',
+                  url: '@url',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    });
+    this.setState({
+      data: data.data,
+    });
   }
 
   render() {
     const { data } = this.state;
+    console.log(data);
     return (
       <main className={styles.archive_wrapper}>
-        <header>2018年</header>
-        <ul>
-          <li>
-            <span>06月 (共3篇文章)</span>
-          </li>
-          <li>
-            <ul>
-              <li>
-                <span className={styles.day}>
-                  25日
-                </span>
-                <a href="https://www.yanceyleo.com/">
-                  Natsu Yasumi~ (1582  / 42 )
-                </a>
-              </li>
-              <li>
-                <span className={styles.day}>
-                  25日
-                </span>
-                <a href="https://www.yanceyleo.com/">
-                  Natsu Yasumi~ (1582  / 42 )
-                </a>
-              </li>
-              <li>
-                <span className={styles.day}>
-                  25日
-                </span>
-                <a href="https://www.yanceyleo.com/">
-                  Natsu Yasumi~ (1582  / 42 )
-                </a>
-              </li>
-            </ul>
-          </li>
-        </ul>
+        {
+          Object.keys(data)
+            .map(key => (
+              <section key={key}>
+                <header className={styles.year}>
+                  {data[key].year}
+                  年
+                </header>
+                <ul className={styles.year_list_wrapper}>
+                  {
+                    Object.keys(data[key].data)
+                      .map(key1 => (
+                        <li key={key1}>
+                          <input id={`tab_${key}_${key1}`} type="checkbox" name="tabs" />
+                        <label htmlFor={`tab_${key}_${key1}`}> {/* eslint-disable-line */}
+                          <span className={styles.month}>
+                            {data[key].data[key1].month}
+                            月
+                      (共
+                            {data[key].data[key1].days.length}
+                            篇文章)
+                          </span>
+                        </label>
+                          <ul className={styles.day_list_container}>
+                            {
+                            Object.keys(data[key].data[key1].days)
+                              .map($key => (
+                                <li className={styles.day_item} key={$key}>
+                                  <span className={styles.day}>
+                                    {data[key].data[key1].days[$key].day}
+                                日
+                                    {' '}
+                                  </span>
+                                  <a href={data[key].data[key1].days[$key].url}>
+                                    {data[key].data[key1].days[$key].title}
+                                    {' '}
+                                    (
+                                    {data[key].data[key1].days[$key].like_count}
+                                    {' '}
+                                    PV
+                                    /
+                                    {' '}
+                                    {data[key].data[key1].days[$key].comment_count}
+                                    {' '}
+                                    Comment
+                                    )
+                                  </a>
+                                </li>
+                              ))
+                          }
+                          </ul>
+                        </li>
+                      ))
+                  }
+                </ul>
+              </section>
+            ))
+        }
       </main>
     );
   }
--- test/test.js
+++ test/test.js
@@ -1,31 +1,55 @@
-// const Mock = require('mockjs');
-//
-// const data = Mock.mock({
-//   'data|20': [
-//     {
-//       poster: '@image(\'500x500\', \'@color()\')',
-//       publish_date: '@date()',
-//       modify_date: '@date()',
-//       title: '@ctitle(6, 18)',
-//       summary: '@cparagraph(3, 7)',
-//       like_count: '@integer(0, 500)',
-//       comment_count: '@integer(0, 500)',
-//       category: '@word(4, 12)',
-//       content: '@cparagraph(10, 20)',
-//       'comments|0-5': [[{
-//         name: '@name',
-//         avatar: '@url',
-//         browser: '@word(10, 18)',
-//         os: '@word(4, 10)',
-//         content: '@cparagraph(1, 4)',
-//         position: '@csentence(5, 12)',
-//       }]],
-//     }],
-// });
-// // 输出结果
-// console.log(JSON.stringify(data, null, 4));
-const arr = ['a', 'b', 'c', 'd', 'e'];
-console.log(arr.indexOf('b')); // 1
+const Mock = require('mockjs');
 
-const str = 'a;b;c;d;e';
-console.log(str.split(';'))
+const data = Mock.mock({
+  'data|2-3': [
+    {
+      year: '20@integer(10, 18)',
+      'data|1-12': [
+        {
+          month: '@integer(1, 12)',
+          'days|0-6': [
+            {
+              day: '@integer(1, 28)',
+              title: '@ctitle(10, 30)',
+              like_count: '@integer(0, 500)',
+              comment_count: '@integer(0, 500)',
+              url: '@url',
+            },
+          ],
+        },
+      ],
+    },
+  ],
+});
+// 输出结果
+console.log(JSON.stringify(data, null, 4));
+
+const demoData = [
+  {
+    year: 2018,
+    data: [
+      {
+        month: 1,
+        days: [
+          {
+            day: 27,
+            title: 'Vue终极教程修订版',
+            url: 'https://www.yanceyleo.com',
+            like_count: 10,
+            comment_count: 100,
+          },
+          {
+            day: 23,
+            title: 'React终极教程修订版',
+            url: 'https://www.yanceyleo.com',
+            like_count: 10,
+            comment_count: 100,
+          },
+          // ...
+        ],
+      },
+      // ...
+    ],
+  },
+  // ...
+];
