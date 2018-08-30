2a630eafd83253bde21dff79daee037692b97035
Yancey <developer@yanceyleo.com>
2018-08-30 23:34:22 +0800

    fix

--- src/components/Widget/Player/player.js
+++ src/components/Widget/Player/player.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 import './player.css';
 import 'aplayer/dist/APlayer.min.css';
 import APlayer from 'aplayer';
-import {
-  GET, POST, PUT, DELETE,
-} from '../../../https/axios';
+import { GET } from '../../../https/axios';
 
 class Player extends Component {
   constructor(props) {
@@ -20,10 +18,6 @@ class Player extends Component {
 
   componentDidMount() {
     this.initAPlayer();
-    // this.getAllBlog();
-    // this.getBlogByTitle();
-    // this.addBlog();
-    // this.updateBlog();
   }
 
   componentWillUnmount() {
@@ -46,48 +40,6 @@ class Player extends Component {
       // todo
     });
   }
-
-  // getAllBlog() {
-  //   GET('/blog', {}).then((res) => {
-  //     console.log(res.data);
-  //   }).catch((error) => {
-  //     console.log(error.message);
-  //   });
-  // }
-  //
-  // getBlogByTitle() {
-  //   GET('/blog/yanceyyanceyyanceyyanceyyancey1', {}).then((res) => {
-  //     console.log(res.data);
-  //   }).catch((error) => {
-  //     console.log(error.message);
-  //   });
-  // }
-  //
-  // addBlog() {
-  //   const params = {
-  //     url: 'https://git.yancey.app/',
-  //     poster: 'https://api.leoyancey.com/api/sayaka.jpg',
-  //     publish_date: '2018-08-09',
-  //     last_modified_date: '2018-08-09',
-  //     title: '山本彩',
-  //     summary: '山本彩山本彩山本彩山本彩山本彩山本彩山本彩山本彩山本彩',
-  //     like_num: 100,
-  //     comment_num: 100,
-  //     category: 'Music',
-  //   };
-  //   POST('/blog', params).then((res) => {
-  //     console.log(res.data);
-  //   });
-  // }
-  //
-  // updateBlog() {
-  //   const params = {
-  //     title: '大声点撒的撒的撒大教堂',
-  //   };
-  //   PUT('/blog/5b6b9dfc025d944092fead44', params).then((res) => {
-  //     console.log(`${res.data}`);
-  //   });
-  // }
 
   render() {
     return (
