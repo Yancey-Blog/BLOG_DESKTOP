8ac87b75f386463c3414fe61074e36cd7ff7bf58
yanceyleo <developer@yanceyleo.com>
2018-10-15 12:06:41 +0800

    switch to online api

--- src/https/axios.js
+++ src/https/axios.js
@@ -11,8 +11,7 @@ axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.headers.put['Content-Type'] = 'application/json';
 
 // config base url
-// axios.defaults.baseURL = 'https://api.leoyancey.com/api/';
-axios.defaults.baseURL = 'http://127.0.0.1:3001/api';
+axios.defaults.baseURL = 'https://api.yanceyleo.com/api/';
 
 
 // config request interceptors
