3bb5a1109d0578da6a5143668cdd4385278b6c77
yanceyleo <developer@yanceyleo.com>
2018-10-16 08:41:14 +0800

    fixbug

--- test/test.js
+++ test/test.js
@@ -1,14 +1,3 @@
-const str = '我是{name}, 我的工作是{job}';
-const obj = {
-  name: 'Yancey',
-  job: 'Web developer',
-};
-
-function tmpl(_str, _obj) {
-  return _str.match(/^{.*?}$/);
-}
-console.log(tmpl(str));
-
 // function isPrime(number) {
 //   if (!(number === +number && /^[0-9]+$/.test(number))) {
 //     return false;
@@ -157,3 +146,119 @@ console.log(tmpl(str));
 // const _newObj = Object.assign({}, obj);
 // _newObj.firstName = 'yancey';
 // console.log(_obj.firstName);
+
+const markyun = {};
+
+markyun.Event = {
+  // 页面加载完成后
+  readyEvent(fn) {
+    if (fn == null) {
+      fn = document;
+    }
+    const oldonload = window.onload;
+    if (typeof window.onload !== 'function') {
+      window.onload = fn;
+    } else {
+      window.onload = function () {
+        oldonload();
+        fn();
+      };
+    }
+  },
+  // 视能力分别使用dom0||dom2||IE方式 来绑定事件
+  // 参数： 操作的元素,事件名称 ,事件处理程序
+  addEvent(element, type, handler) {
+    if (element.addEventListener) {
+      // 事件类型、需要执行的函数、是否捕捉
+      element.addEventListener(type, handler, false);
+    } else if (element.attachEvent) {
+      element.attachEvent(`on${type}`, () => {
+        handler.call(element);
+      });
+    } else {
+      element[`on${type}`] = handler;
+    }
+  },
+  // 移除事件
+  removeEvent(element, type, handler) {
+    if (element.removeEventListener) {
+      element.removeEventListener(type, handler, false);
+    } else if (element.datachEvent) {
+      element.detachEvent(`on${type}`, handler);
+    } else {
+      element[`on${type}`] = null;
+    }
+  },
+  // 阻止事件 (主要是事件冒泡，因为IE不支持事件捕获)
+  stopPropagation(ev) {
+    if (ev.stopPropagation) {
+      ev.stopPropagation();
+    } else {
+      ev.cancelBubble = true;
+    }
+  },
+  // 取消事件的默认行为
+  preventDefault(event) {
+    if (event.preventDefault) {
+      event.preventDefault();
+    } else {
+      event.returnValue = false;
+    }
+  },
+  // 获取事件目标
+  getTarget(event) {
+    return event.target || event.srcElement;
+  },
+  // 获取event对象的引用，取到事件的所有信息，确保随时能使用event；
+  getEvent(e) {
+    let ev = e || window.event;
+    if (!ev) {
+      let c = this.getEvent.caller;
+      while (c) {
+        ev = c.arguments[0];
+        if (ev && Event == ev.constructor) {
+          break;
+        }
+        c = c.caller;
+      }
+    }
+    return ev;
+  },
+};
+
+function deepClone(initalObj, finalObj) {
+  var obj = finalObj || {};
+  for (var i in initalObj) {
+    var prop = initalObj[i];        // 避免相互引用对象导致死循环，如initalObj.a = initalObj的情况
+    if(prop === obj) {
+      continue;
+    }
+    if (typeof prop === 'object') {
+      obj[i] = (prop.constructor === Array) ? [] : {};
+      arguments.callee(prop, obj[i]);
+    } else {
+      obj[i] = prop;
+    }
+  }
+  return obj;
+}
+var str = {};
+var obj = { a: {a: "hello", b: 21} };
+deepClone(obj, str);
+console.log(str.a);
+
+function deepClone(initalObj, finalObj) {
+  var obj = finalObj || {};
+  for (var i in initalObj) {
+    var prop = initalObj[i];        // 避免相互引用对象导致死循环，如initalObj.a = initalObj的情况
+    if(prop === obj) {
+      continue;
+    }
+    if (typeof prop === 'object') {
+      obj[i] = (prop.constructor === Array) ? [] : Object.create(prop);
+    } else {
+      obj[i] = prop;
+    }
+  }
+  return obj;
+}
