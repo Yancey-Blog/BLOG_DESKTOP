e9e1300bc03cb779d5fec77dfddc2d2b5f7ad628
yanceyleo <developer@yanceyleo.com>
2019-01-15 21:15:25 +0800

    modify styles

--- src/components/Common/Footer/Footer.tsx
+++ src/components/Common/Footer/Footer.tsx
@@ -28,32 +28,34 @@ class Footer extends React.Component<{}, {}> {
   public render() {
     return (
       <footer className={styles.yancey_common_footer}>
-        <section>
-          <p className={styles.creator}>
-            Brought to you with
-            <svg className={styles.icon_heart}>
-              <use xlinkHref={`${svgIcons}${svgSprite.heart}`} />
-            </svg>
-            by Yancey
-          </p>
-        </section>
-        <hr className={styles.dot_split} />
-        <section className={styles.copyright_wrapper}>
-          <p className={styles.copyright}>
-            Copyright &copy; {new Date().getFullYear()} Yancey Inc. All rights
-            reserved.
-          </p>
-          <ul className='copyright_list'>
-            {Object.keys(copyright).map(key => (
-              <li className={styles.copyright_item} key={key}>
-                <Link to={copyright[key].url}>{copyright[key].name}</Link>
+        <div className={styles.footer_container}>
+          <section>
+            <p className={styles.creator}>
+              Brought to you with
+              <svg className={styles.icon_heart}>
+                <use xlinkHref={`${svgIcons}${svgSprite.heart}`} />
+              </svg>
+              by Yancey
+            </p>
+          </section>
+          <hr className={styles.dot_split} />
+          <section className={styles.copyright_wrapper}>
+            <p className={styles.copyright}>
+              Copyright &copy; {new Date().getFullYear()} Yancey Inc. All rights
+              reserved.
+            </p>
+            <ul className='copyright_list'>
+              {Object.keys(copyright).map(key => (
+                <li className={styles.copyright_item} key={key}>
+                  <Link to={copyright[key].url}>{copyright[key].name}</Link>
+                </li>
+              ))}
+              <li className={styles.copyright_item}>
+                <a href={socialMedia.email.url}>Contact</a>
               </li>
-            ))}
-            <li className={styles.copyright_item}>
-              <a href={socialMedia.email.url}>Contact</a>
-            </li>
-          </ul>
-        </section>
+            </ul>
+          </section>
+        </div>
       </footer>
     );
   }
