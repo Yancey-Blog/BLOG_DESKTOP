5dbb62fe6c94a61f018269d9db469e229839aff1
yanceyleo <developer@yanceyleo.com>
2019-01-16 17:40:54 +0800

    add alias support and optimize footer style

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -150,6 +150,8 @@ module.exports = {
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
+      '@assets': path.resolve(__dirname, '../src/assets'),
+      '@constant': path.resolve(__dirname, '../src/constant'),
     },
     plugins: [
       // Adds support for installing with Plug'n'Play, leading to faster installs and adding
