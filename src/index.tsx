8115638662d3f677cf5a1675adc9846952f6e391
yanceyleo <developer@yanceyleo.com>
2019-04-04 17:45:55 +0800

    fix: Try uninstall ServiceWorker

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { unregister as unregisterServiceWorker } from './registerServiceWorker'
 
 ReactDOM.render(
   <BrowserRouter>
@@ -10,4 +10,4 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById('root') as HTMLElement,
 );
-registerServiceWorker();
+unregisterServiceWorker();
